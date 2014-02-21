8121ee8c15b26241754c65635618568d596b312f
root <root@ubuntu.(none)>
2014-02-20 22:57:16 -0500

    clean up

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
-from tracker.models import Beer 
-from tracker.models import Brewery 
-from tracker.models import Style 
-from tracker.models import Taster 
-from tracker.models import Rating 
+from tracker.models import Beer, Brewery, Style, Taster, Rating 
 
-admin.site.register(Beer)
-admin.site.register(Brewery)
-admin.site.register(Style)
-admin.site.register(Taster)
-admin.site.register(Rating)
+admin.site.register(Beer, Brewery, Style, Taster, Rating)
 
 # Register your models here.
