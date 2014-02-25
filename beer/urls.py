e1a5374fd8ab5563cc2f73676df3c6dd4f481f34
Alex <alex.billett@gmail.com>
2014-02-25 17:18:56 -0500

    got rid of foundation app import css from static folder instead

--- beer/urls.py
+++ beer/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tracker/', include('tracker.urls')),
     url(r'^advocate/$', 'tracker.views.advocate'),
-)
+    url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/apps/static'}),
+) #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tracker/urls.py
+++ tracker/urls.py
@@ -3,7 +3,7 @@ from tracker import views
 
 urlpatterns = patterns('',
     url(r'^$',views.index,name='index'),
-    url(r'^/beer/(?P<beer_id>\d+)/$', views.beerDetail, name='beerDetail'),
+    url(r'^beer/(?P<beer_id>\d+)/$', views.beerDetail, name='beerDetail'),
     url(r'^(?P<poll_id>\d+)/$', views.beerDetail, name='beerDetail'),
     url(r'^(?P<beer_id>\d+)/beer/$', views.beerDetail, name='beerDetail'),
 )
