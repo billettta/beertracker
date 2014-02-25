0a8278f014bd2a13867f7f0936001ffd93eee29f
Alex <alex.billett@gmail.com>
2014-02-24 21:37:09 -0500

    added quote model, added nav bar to beerdetail

--- tracker/models.py
+++ tracker/models.py
@@ -30,15 +30,18 @@ class Beer(models.Model):
     #uploads to MEDIA_ROOT/beershots need to serve from apache or S3(eventually)
     #need to figure out how to rename to name of beer and put in folder for brewery
     picture = models.ImageField(upload_to='beershots')
+    # additional fields: average rating ?
     def __unicode__(self):
         return self.name
 
 class Taster(models.Model):
+#    user = models.OneToOneField(User)
+    description = models.TextField(blank=True)
+    nickName = models.CharField(max_length=250,blank=True)
+    # think these might be able to be pulled in from generic user class
     first = models.CharField(max_length=200)
     last = models.CharField(max_length=200)
-    nickName = models.CharField(max_length=250,blank=True)
     email = models.EmailField(max_length=254)
-    description = models.TextField(blank=True)
     def __unicode__(self):
         return self.nickName
 
@@ -49,8 +52,16 @@ class Rating(models.Model):
     overallRating = models.DecimalField(max_digits=3,decimal_places=1)
     volumeRating = models.DecimalField(max_digits=3,decimal_places=1,blank=True)
     notes = models.TextField(blank=True)
+    # right now this is calculated when called instead of stored in DB probably want to switch so its only calculated once
     def _get_drunk(self):
         "Returns the drunkability"
         return (beer.abv * self.VolumeRating)/10
     drunkRating = property(_get_drunk)
 
+class Quote(models.Model):
+    authors = models.ManyToManyField(Taster)
+    quoteText = models.TextField()
+    #should be taken from logged in user
+#    submiter = models.ForeignKey(Taster)
+
+#additional objects: team ?
--- tracker/views.py
+++ tracker/views.py
@@ -7,21 +7,30 @@ def index(request):
 
 def beerDetail(request, beer_id):
     beer = get_object_or_404(Beer, pk=beer_id)
+    #should limit for 10 or so with pagination
     ratings = Rating.objects.filter(beer=beer_id)
     return render(request, 'tracker/beerDetail.html',{'beer':beer,'rating_list':ratings})
 
 def breweryDetail(request, brewery_id):
-    brewery = get_object_or_404(Beer, pk=brewery_id)
+    brewery = get_object_or_404(Brewery, pk=brewery_id)
+    #should limit for 10 or so with pagination
+    beers = Beer.objects.filter(brewery=brewery_id)
     return render(request, 'tracker/breweryDetail.html',{'brewery',brewery})
 
 def ratingDetail(request, rating_id):
-    rating = get_object_or_404(Beer, pk=rating_id)
+    rating = get_object_or_404(Rating, pk=rating_id)
     return render(request, 'tracker/ratingDetail.html',{'rating',rating})
 
 def styleDetail(request, style_id):
     style = get_object_or_404(Beer, pk=style_id)
     return render(request, 'tracker/styleDetail.html',{'style',style})
 
+def myRatings(request,tracker_id):
+    #should probably actually look up on logged in user
+    ratings  = Ratigns.objects.filter(tracker=tracker_id)
+    return render(request, 'tracker/ratingsList.html',{'ratings',ratings})
+
+#do we actually need a style list view ?
 def styleList(request):
     return HttpResponse("You're looking at beer %s.")
 
