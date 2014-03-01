9dba144fc27835ed945293906ba25547c27559c3
Dree <alex.dreelin@gmail.com>
2014-03-01 12:25:45 -0500

    made beer table a template

--- tracker/templatetags/beer_tags.py
+++ tracker/templatetags/beer_tags.py
@@ -6,6 +6,14 @@ register = template.Library()
 def subtract(value, arg):
     return value - arg
 
+@register.inclusion_tag('tracker/beerTable.html')
+def beerTable(beers, **kwargs):
+    if kwargs['orderBy'] != "":
+        orderBy = kwargs['orderBy']
+    else:
+        orderBy = kwargs['defaultOrderBy']
+    return {'beers' : beers, 'orderBy' : orderBy }
+
 @register.inclusion_tag('tracker/paginatorNumbers.html')
 def paginateNumbers(currentPage, **kwargs):
     if kwargs['orderBy'] != "":
