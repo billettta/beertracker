ee4771d0dc7ca86ebb26d80f21a7f451674a23b2
Dree <alex.dreelin@gmail.com>
2014-03-01 11:48:57 -0500

    made paginator number template_tag

--- tracker/templatetags/beer_tags.py
+++ tracker/templatetags/beer_tags.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter    
 def subtract(value, arg):
     return value - arg
+
+@register.inclusion_tag('tracker/paginatorNumbers.html')
+def paginateNumbers(currentPage, **kwargs):
+    if kwargs['orderBy'] != "":
+        orderBy = kwargs['orderBy']
+    else:
+        orderBy = kwargs['defaultOrderBy']
+    return {'activePage' : currentPage, 'orderBy' : orderBy }
